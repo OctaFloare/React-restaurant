d224075bf70d2240d1fb45fcc4af2927f948bf9a
floare <adifloare1969@gmail.com>
2021-03-01 13:19:35 +0200

    [react-restaurant] Follow-up change misleading icon

--- src/core/navbar/navbar.jsx
+++ src/core/navbar/navbar.jsx
@@ -1,5 +1,5 @@
-import {AppBar, Toolbar, Typography} from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu'
+import {AppBar, Toolbar} from '@material-ui/core'
+import HomeIcon from '@material-ui/icons/Home'
 import {RestaurantMenu} from "@material-ui/icons";
 import {NavbarIcons} from "./navbar-icons/navbar-icons";
 import {NavbarTitle} from "./navbar-title/navbar-title";
@@ -8,7 +8,7 @@ import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 export const NavBar = () =>
     <AppBar position ='static'>
         <Toolbar variant ='dense'>
-            <NavbarIcons link={'/'} icon={<MenuIcon/>} />
+            <NavbarIcons link={'/'} icon={<HomeIcon/>} />
             <NavbarIcons link={'/menu'} icon={<RestaurantMenu/>} />
             <NavbarTitle text={'React Restaurant'} />
             <NavbarIcons link={'/menu/shopping-cart'} icon={<AddShoppingCartIcon/>} edge={'end'} />
