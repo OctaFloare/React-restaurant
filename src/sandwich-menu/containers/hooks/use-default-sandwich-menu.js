56f1b7c6ef9163fd3b16d0fea596fb6e0016a982
floare <adifloare1969@gmail.com>
2021-03-02 10:38:54 +0200

    [react-restaurant] Follow-up remove unused context variables

--- src/sandwich-menu/containers/hooks/use-default-sandwich-menu.js
+++ src/sandwich-menu/containers/hooks/use-default-sandwich-menu.js
@@ -1,7 +1,3 @@
-import {useState} from "react";
-
 export const useDefaultSandwichMenu = () => {
-    const [data, setData] = useState([])
-    const [shoppingCartData, setShoppingCartData] = useState([])
-    return {data, setData , shoppingCartData, setShoppingCartData}
+    return []
 }
