170fc7c32c966b468f106e6dd9d8679d326ecb8d
floare <adifloare1969@gmail.com>
2021-03-09 10:40:50 +0200

    [react-restaurant] Follow-up remove line break

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {BrowserRouter as Router} from "react-router-dom";
 import {Provider} from 'react-redux'
 import {store} from "./core/store/store"
 
-
 const App = () =>
     <Provider store={store}>
         <Router>
