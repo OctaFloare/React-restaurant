267932eca36505e435961da61b203fae6980ea61
floare <adifloare1969@gmail.com>
2021-03-09 10:39:55 +0200

    [react-restaurant] Follow-up added key to mapping

--- src/menu/containers/food-cards/food-cards.jsx
+++ src/menu/containers/food-cards/food-cards.jsx
@@ -5,7 +5,7 @@ import {FoodCard} from "../food-card/food-card";
 export const FoodCards = () =><Box mr={5} mb={2} ml={5}>
     <Grid container direction='row' justify='space-around' spacing={8}>
         {useMenuContext().data.map(foodItem =>
-            <Grid item xs={12} sm={12} md={12} lg={3}>
+            <Grid item xs={12} sm={12} md={12} lg={3} key={foodItem.id}>
                 <FoodCard foodItem={foodItem} />
             </Grid>
         )}
